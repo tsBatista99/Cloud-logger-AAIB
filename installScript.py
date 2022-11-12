f4f36132a9583ab7b5a3968ca14324858eacd64b
Tiago Batista <117983623+tsBatista99@users.noreply.github.com>
2022-11-12 16:01:57 +0000

    Add files via upload

--- installScript.py
+++ installScript.py
@@ -3,14 +3,14 @@
 Script to install packages
 """
 ##To install streamlit
-pip3 install streamlit
+sudo pip3 install streamlit
 ##To install MQTT service
-apt install -y mosquitto
-apt install mosquitto-clients
-service mosquitto start
-service mosquitto status
+sudo apt install -y mosquitto
+sudo apt install mosquitto-clients
+sudo service mosquitto start
+sudo service mosquitto status
 ##To install Paho-MQTT
-pip3 install paho-mqtt
+sudo pip3 install paho-mqtt
 git clone https://github.com/eclipse/paho.mqtt.python.git
 cd paho.mqtt.python
 python setup.py install
