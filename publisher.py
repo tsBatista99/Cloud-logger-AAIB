f0181fd3cceeb7c05e77d7ef185e4a3f54902910
Tiago Batista <117983623+tsBatista99@users.noreply.github.com>
2022-11-15 22:06:59 +0000

    Add files via upload

--- publisher.py
+++ publisher.py
@@ -2,12 +2,14 @@ import paho.mqtt.client as mqtt
 from random import uniform
 import time
 
+
 mqttBroker ="test.mosquitto.org" 
 
 client = mqtt.Client("test/AAIB")
 client.connect(mqttBroker, port=1883) 
 
-randNumber = uniform(20.0, 21.0)
-client.publish("Number", randNumber)
-print("Just published " + str(randNumber) + " to topic Random")
-time.sleep(1)
+while True:
+    randNumber = uniform(20.0, 21.0)
+    client.publish("NumberAAIB", randNumber)
+    print("Just published " + str(randNumber) + " to topic NumberAAIB")
+    time.sleep(1)
--- subscriber.py
+++ subscriber.py
@@ -3,16 +3,23 @@ import time
 
 def on_message(client, userdata, message):
     print("received message: " ,str(message.payload.decode("utf-8")))
+    
+def disconnect():
+    print("client is disconnecting..")
+    client.disconnect()
+
 
 mqttBroker ="test.mosquitto.org"
 
-client = mqtt.Client("test/AAIB")
+client = mqtt.Client("Gitpod")
 client.connect(mqttBroker, port=1883) 
 
 client.loop_start()
 
-client.subscribe("Number")
+client.subscribe("NumberAAIB")
 client.on_message=on_message 
 
-time.sleep(30)
+time.sleep(30) #colocar 10 minutos de aquisição
 client.loop_stop()
+disconnect()
+
